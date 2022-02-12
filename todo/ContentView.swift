a6ae36d02b958902ac90211ba160a0151566346e
Atharva Parkhe <patharv777@gmail.com>
2022-02-12 11:59:53 +0530

    Initial Commit

---
+++ todo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todo
+//
+//  Created by Atharva Parkhe on 12/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
