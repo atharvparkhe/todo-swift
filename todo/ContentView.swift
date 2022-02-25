47f35833f53e99de7685bab9e3cf055a7dc8febc
Atharva Parkhe <patharv777@gmail.com>
2022-02-26 00:36:33 +0530

    initial commit

--- todo/ContentView.swift
+++ todo/ContentView.swift
@@ -1,16 +1,40 @@
-//
-//  ContentView.swift
-//  todo
-//
-//  Created by Atharva Parkhe on 12/02/22.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject private var todoListVM = TodoListViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            VStack{
+                Form{
+                    Section(header: Text("Add Todo")){
+                        HStack{
+                            TextField("New Task", text: $todoListVM.newTask)
+                            Button {
+                                todoListVM.addTodoItem()
+                                todoListVM.populateTodoItems()
+                            } label: {
+                                Text("ADD TODO")
+                            }
+                        }
+                    }
+                }.frame(height: 120)
+                Spacer()
+
+                Section(header: Text("Todo's")){
+                    List(todoListVM.todoItems, id:\.id){ todoItem in
+                        HStack{
+                            Text(todoItem.task)
+                        }
+                    }
+                }.onAppear{
+                    todoListVM.populateTodoItems()
+                }
+                Spacer()
+            }.navigationTitle("Todo Application")
+            
+        }
     }
 }
 
